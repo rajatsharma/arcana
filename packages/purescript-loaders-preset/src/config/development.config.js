f03227a297963bdb44f73cc4037a8ed699e48318
Rajat Sharma <lunasunkaiser@gmail.com>
2018-12-10 23:52:30 +0530

    Config fixes

--- packages/purescript-loaders-preset/src/config/development.config.js
+++ packages/purescript-loaders-preset/src/config/development.config.js
@@ -1,12 +1,17 @@
 module.exports = options => ({
-  test: /\.purs$/,
-  loader: require.resolve('purs-loader'),
-  exclude: [/[/\\\\]node_modules[/\\\\]/],
-  query: {
-    psc: 'psa',
-    src: [options.pursFiles, options.pscPackages],
-    pscPackage: true,
-    bundle: false,
-    watch: true,
+  module: {
+    rules: {
+      test: /\.purs$/,
+      loader: require.resolve('purs-loader'),
+      exclude: [/[/\\\\]node_modules[/\\\\]/],
+      query: {
+        psc: 'psa',
+        src: [options.pursFiles, options.pscPackages],
+        pscPackage: true,
+        bundle: false,
+        watch: true,
+      },
+    },
   },
+  plugins: [],
 });
